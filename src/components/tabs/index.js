ce4e00c9113613274eeafa956c72eba8ea198b31
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2024-01-16 09:31:06 +0100

    update

--- src/components/tabs/index.js
+++
@@ -1,5 +0,0 @@
-import Tab from './Tab.svelte';
-import Tabs from './Tabs.svelte';
-
-Tabs.Tab = Tab;
-export default Tabs;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,55 @@
-<script>
-	import Main from '$layout/Main.svelte';
-	import MainNavbar from '$layout/MainNavbar.svelte';
-	// import OnThisPage from '$layout/OnThisPage.svelte';
+<div class="drawer lg:drawer-open">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<!-- Page content here -->
 
-	import Blog from '$lib/Blog.svelte';
-	import Update from '$lib/Update.svelte';
+		<div role="tablist" class="tabs tabs-bordered w-full">
+			<input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Tab 1" />
+			<div role="tabpanel" class="tab-content p-10">
+				Lorem ipsum dolor sit amet c o n s e c t e t u r a d i p i s c i n g e l i t. A b a d i p i
+				s i c i n g e l i t. A b a a c c u s a m u s a l i q u i p e x e a c o m m o d o c o n s e q
+				u a t. Lorem ipsum dolor sit amet c o n s e c t e t u r a d i p i s c i n g e l i t. A b a d
+				i p i s i c i n g e l i t. A b a a c c u s a m u s a l i q u i p e x e a c o m m o d o c o n
+				s e q u a t. Lorem ipsum dolor sit amet c o n s e c t e t u r a d i p i s c i n g e l i t. A
+				b a d i p i s i c i n g e l i t. A b a a c c u s a m u s a l i q u i p e x e a c o m m o d o
+				c o n s e q u a t. Lorem ipsum dolor sit amet c o n s e c t e t u r a d i p i s c i n g e l
+				i t. A b
+			</div>
 
-	import UI from '$/ui';
-	import Tabs from '$/tabs';
-</script>
+			<input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Tab 2" checked />
+			<div role="tabpanel" class="tab-content p-10">Tab content 2</div>
 
-<main>
-	<Main>
-		<MainNavbar slot="navbar-left" />
+			<input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Tab 3" />
+			<div role="tabpanel" class="tab-content p-10">Tab content 3</div>
+		</div>
 
-		<Tabs defaultTab="Home">
-			<Tabs.Tab title="Home" icon="ic:baseline-home">
-				<UI.Title>School of Students</UI.Title>
-				<UI.Text>This is a</UI.Text>
-			</Tabs.Tab>
-			<Tabs.Tab title="Blog" icon="ic:outline-comment">
-				<UI.Title>Blog</UI.Title>
-				<Blog />
-			</Tabs.Tab>
-			<Tabs.Tab title="Updates" icon="ic:round-update">
-				<UI.Title>Updates</UI.Title>
-				<Update />
-			</Tabs.Tab>
-			<Tabs.Tab title="About" icon="ic:outline-info">
-				<UI.Title>About</UI.Title>
-				<UI.Text>TODO: Fill this</UI.Text>
-			</Tabs.Tab>
-		</Tabs>
-	</Main>
-</main>
+		<button class="btn">Hello daisyUI</button>
+
+		<button class="btn btn-primary">Button</button>
+
+		<details class="dropdown">
+			<summary class="m-1 btn">open or close</summary>
+			<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+				<li><a>Item 1</a></li>
+				<li><a>Item 2</a></li>
+			</ul>
+		</details>
+
+		<ul class="steps steps-vertical">
+			<li class="step step-primary">Register</li>
+			<li class="step step-primary">Choose plan</li>
+			<li class="step">Purchase</li>
+			<li class="step">Receive Product</li>
+		</ul>
+
+		<label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+			<!-- Sidebar content here -->
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
+</div>
