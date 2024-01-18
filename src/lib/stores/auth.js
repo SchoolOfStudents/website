3a198de9c57a436d24ef9e3275137b1e649a90d5
Sau1707 <leonardo@saurwein.ch>
2024-01-18 09:20:59 +0100

    Add working login system

--- src/lib/stores/auth.js
+++ src/lib/stores/auth.js
@@ -1,7 +1,60 @@
-import { account } from './appwrite';
+// import { account } from './appwrite';
 import { writable } from 'svelte/store';
+import { account } from '$stores/appwrite';
+
+const createAuthState = () => {
+    const { subscribe, set } = writable({
+        username: null,
+        email: null,
+    });
+
+    return {
+        subscribe,
+        update: async () => {
+            try {
+                const user = await account.get();
+                console.log(user);
+                set({ username: user.name, email: user.email });
+            }
+            catch (error) {
+                console.log('Not logged in');
+            }
+
+        },
+        user: async () => {
+            return await account.get();
+        },
+        setUsername: async (username) => {
+            set({ username: username, email: null });
+        },
+        logout: async () => {
+            try {
+                await account.deleteSession('current');
+                set({ username: null, email: null });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        /* All the login methods */
+        loginWithGitHub: () => {
+            try {
+                account.createOAuth2Session('github', window.location.href, window.location.href);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+};
 
 
+export const auth = createAuthState();
+
+
+//export const auth = writable(null);
+
+
+/*
 const createState = () => {
     const { subscribe, set } = writable({
         username: null,
@@ -34,6 +87,4 @@ const createState = () => {
         }
     };
 };
-
-
-export const auth = createState();
+*/
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,43 +1,14 @@
 <script>
-	import { onMount } from 'svelte';
-	import { account, ID } from '$stores/appwrite';
+	import { auth } from '$stores/auth';
 
-	let isLoggedIn = false;
-	let userName = '';
+	$: username = $auth.username;
 
-	onMount(async () => {
-		try {
-			const response = await account.get();
-			isLoggedIn = true;
-			console.log(response);
-			userName = response.name;
-		} catch (error) {
-			console.log('Not logged in');
-		}
-	});
-
-	function loginWithGitHub() {
-		account
-			.createOAuth2Session('github', 'http://localhost:5173/login', 'http://localhost:5173/login')
-			.catch((error) => {
-				console.error('OAuth login failed:', error);
-			});
-	}
-
-	async function logout() {
-		try {
-			await account.deleteSession('current');
-			isLoggedIn = false;
-			userName = '';
-		} catch (error) {
-			console.error('Logout failed:', error);
-		}
-	}
+	console.log(auth);
 </script>
 
-{#if isLoggedIn}
-	<p>Welcome, {userName}!</p>
-	<button on:click={logout}>Logout</button>
+{#if typeof username !== 'object'}
+	<p>Welcome, {username}!</p>
+	<button on:click={auth.logout}>Logout</button>
 {:else}
-	<button on:click={loginWithGitHub}>Login with GitHub</button>
+	<button on:click={auth.loginWithGitHub}>Login with GitHub</button>
 {/if}
